3c9b506a099757d182aaa3ecfe2f44a33a3c5a7f
Artem <artem.belykh.ru@gmail.ru>
2023-09-14 22:59:52 +0200

    second example

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// S = t ** 3 - 3 * (t ** 2) + 2
+// тогда ↓
+// V = 3 * t ** 2 - 6 * t 
+
+Console.Write("Введите t: ");
+double t = Convert.ToDouble(Console.ReadLine());
+double V = 3 *  Math.Pow(t, 2) - 6 * t;
+Console.WriteLine($"V = {V:F2}");
