6e31c956670611b83589aeec58d85c310f31b4c5
Artem468 <56316884+Artem468@users.noreply.github.com>
2023-09-15 09:29:40 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,8 +1,14 @@
-﻿// S = t ** 3 - 3 * (t ** 2) + 2
-// тогда ↓
-// V = 3 * t ** 2 - 6 * t 
+// Высокий уровень
 
-Console.Write("Введите t: ");
-double t = Convert.ToDouble(Console.ReadLine());
-double V = 3 *  Math.Pow(t, 2) - 6 * t;
-Console.WriteLine($"V = {V:F2}");
+Console.Write("Введите x: ");
+double x = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите a: ");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите c: ");
+double c = Convert.ToDouble(Console.ReadLine());
+
+double numeratorL = Math.Sqrt(Math.Exp(4) - Math.Pow(Math.Cos(Math.Pow(x, 2) * Math.Pow(a, 5)), 4)) + Math.Pow(Math.Atan(a - Math.Pow(x, 5)), 4);
+double denominatorL = Math.E * Math.Sqrt(Math.Abs(a + x * Math.Pow(c, 4)));
+
+double L = numeratorL / denominatorL;
+Console.WriteLine($"L = {L:F2}");
